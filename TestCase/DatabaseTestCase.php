dde96cf0f0bafe955d30854e20a35195b13fda8f
Marc Aschmann <marc@aschmann.org>
2013-12-28 14:01:42 +0100

    cleanup

--- TestCase/DatabaseTestCase.php
+++ TestCase/DatabaseTestCase.php
@@ -4,7 +4,9 @@
  */
 namespace Asm\TranslationLoaderBundle\TestCase;
 
-use Symfony\Bundle\FrameworkBundle\Tests\Functional\WebTestCase;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Component\Console\Tester\CommandTester;
+use Symfony\Bundle\FrameworkBundle\Console\Application;
 use Doctrine\Common\DataFixtures\Loader;
 use Doctrine\Common\DataFixtures\Purger\ORMPurger;
 use Doctrine\Common\DataFixtures\Executor\ORMExecutor;
@@ -14,26 +16,32 @@ use Doctrine\Common\DataFixtures\Executor\ORMExecutor;
  *
  * @package Asm\TranslationLoaderBundle\TestCase
  * @author marc aschmann <maschmann@gmail.com>
- * @uses Symfony\Bundle\FrameworkBundle\Tests\Functional\WebTestCase
+ * @uses Symfony\Bundle\FrameworkBundle\Test\WebTestCase
  * @uses Doctrine\Common\DataFixtures\Loader
  * @uses Doctrine\Common\DataFixtures\Purger\ORMPurger
  * @uses Doctrine\Common\DataFixtures\Executor\ORMExecutor
  */
 abstract class DatabaseTestCase extends WebTestCase
 {
+    /**
+     * @var
+     */
+    private static $application;
+
 
     /**
      * initialize fixtures and stuff
      */
     public function setUp()
     {
-        self::$kernel = $this->createKernel();
-        self::$kernel->boot();
+        parent::setUp();
+
+        $this->createKernel();
 
         // initialize database and generate our tables
-        self::runCommand('doctrine:database:create');
-        self::runCommand('doctrine:schema:update --force');
-        self::runCommand('doctrine:fixtures:load -n --purge-with-truncate');
+        $this->runCommand('doctrine:database:create');
+        $this->runCommand('doctrine:schema:update', array('--force' => true));
+        $this->runCommand('doctrine:fixtures:load', array('-n', '--purge-with-truncate' => true));
 
         // prepare database and load fixtures
         $em     = self::$kernel->getContainer()->get('doctrine')->getManager();
@@ -51,5 +59,38 @@ abstract class DatabaseTestCase extends WebTestCase
     public function getContainer()
     {
         return self::$kernel->getContainer();
+    }
+
+
+    /**
+     * run a symfony command with CommandTester wrapper
+     *
+     * @param string $command command to test
+     * @param array $params parameters for command
+     * @return \Asm\TranslationLoaderBundle\TestCase\Symfony\Component\Console\Tester\CommandTester
+     */
+    public function runCommand($command, array $params=array())
+    {
+        /** @var \Symfony\Bundle\FrameworkBundle\Console\Application $application */
+        $application = $this->initApplication();
+        // using command tester here, because it's quite easy and I need it later...
+        /** @var Symfony\Component\Console\Tester\CommandTester $commandTester */
+        $commandTester = new CommandTester($application->find($command));
+        $commandTester->execute($params);
+
+        return $commandTester;
+    }
+
+
+    /**
+     * initialize application with kernel
+     */
+    private function initApplication()
+    {
+        if (empty(self::$application)) {
+            self::$application = new Application(self::$kernel);
+        }
+
+        return self::$application;
     }
 }
