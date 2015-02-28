167fec7515bc5e2ff3093a28d7eb9b479c45f5b1
Marc Aschmann <marc@aschmann.org>
2015-02-28 22:42:13 +0100

    Fixing node

--- Gulpfile.js
+++ Gulpfile.js
@@ -6,129 +6,51 @@ var gulp = require('gulp'),
             'gulp-strip-css-comments': 'stripComments'
         }
     }),
-    theme = 'default',
-    env = 'dev',
-    minify = false,
     files = {
         js: {
-            bootstrap: [
-                './vendor/bower-asset/jquery/dist/jquery.js',
-                './vendor/bower-asset/jquery-ui/jquery-ui.js',
-                './vendor/npm-asset/bootstrap/dist/js/bootstrap.js'
-            ],
             core: [
-                './src/MxBundle/Resources/public/js/main.js',
-                './src/MxBundle/Resources/public/js/mustache.js',
-                './src/MxBundle/Resources/public/js/plugins.js'
+                './vendor/asm/translation-loader-bundle/Resources/public/js/main.js',
+                './vendor/asm/translation-loader-bundle/Resources/public/js/vendor/mustache.js',
+                './vendor/asm/translation-loader-bundle/Resources/public/js/plugins.js'
             ],
-            user: [
-                './src/MxBundle/Resources/public/js/user.*.js'
-            ]
+            destination: './web/bundles/asmtranslationloader/js/min/'
         },
         css: {
-            bootstrap: [
-                './vendor/npm-asset/bootstrap/dist/css/bootstrap.css',
-                './vendor/npm-asset/bootstrap/dist/css/bootstrap-theme.css',
-                './vendor/bower-asset/jquery-ui/themes/ui-darkness/jquery-ui.css',
-                './vendor/bower-asset/jquery-ui/themes/ui-darkness/theme.css'
-            ],
             core: [
-                './src/MxBundle/Resources/stylus/main.styl'
+                './vendor/asm/translation-loader-bundle/Resources/stylus/core.styl'
             ],
-            user: [
-                './src/MxBundle/Resources/stylus/user.*.styl'
-            ]
+            destination: './web/bundles/asmtranslationloader/css/min/'
         }
     };
-
-/**
- * check for commandline params and define defaults
- */
-if (plugins.util.env.env && plugins.util.env.env == 'prod') {
-    minify = true;
-    errorlog = false;
-    env = 'prod';
-}
 
 console.log(plugins);
 gulp.task('handle-assets', ['core-js', 'core-css']);
 
 gulp.task('default', ['core-js', 'core-css'], function(){
-    gulp.watch(files.css.bootstrap, ['build-css-bootstrap']);
     gulp.watch(files.css.core, ['build-css-core']);
-    gulp.watch(files.css.user, ['build-css-user']);
-    gulp.watch(files.js.bootstrap,['build-js-bootstrap']);
     gulp.watch(files.js.core,['build-js-core']);
-    gulp.watch(files.js.user,['build-js-user']);
 });
 
-gulp.task('core-css', ['build-css-bootstrap', 'build-css-core', 'build-css-user']);
-gulp.task('core-js', ['build-js-bootstrap', 'build-js-core', 'build-js-user']);
-
-gulp.task('build-css-bootstrap', function () {
-    return gulp.src(files.css.bootstrap)
-        .pipe(plugins.plumber())
-        .pipe(plugins.sourcemaps.init())
-            .pipe(plugins.if(minify, plugins.uglifycss()))
-            .pipe(plugins.concat('bootstrap.css'))
-            .pipe(plugins.stripComments({all: true}))
-        .pipe(plugins.sourcemaps.write('./'))
-        .pipe(gulp.dest('./web/css/' + env));
-});
+gulp.task('core-css', ['build-css-core']);
+gulp.task('core-js', ['build-js-core']);
 
 gulp.task('build-css-core', function () {
     return gulp.src(files.css.core)
         .pipe(plugins.plumber())
         .pipe(plugins.sourcemaps.init())
-            .pipe(plugins.stylus({sourceComments: 'map', compress: minify}))
+            .pipe(plugins.stylus({sourceComments: 'map', compress: true}))
             .pipe(plugins.concat('core.css'))
         .pipe(plugins.sourcemaps.write('./'))
-        .pipe(gulp.dest('./web/css/' + env));
-});
-
-gulp.task('build-css-user', function () {
-    return gulp.src(files.css.user)
-        .pipe(plugins.plumber())
-        .pipe(plugins.sourcemaps.init())
-            .pipe(plugins.stylus({sourceComments: 'map', compress: minify}))
-            .pipe(plugins.concat('user.css'))
-        .pipe(plugins.sourcemaps.write('./'))
-        .pipe(gulp.dest('./web/css/' + env));
-});
-
-gulp.task('build-js-bootstrap', function() {
-    return gulp.src(files.js.bootstrap)
-        .pipe(plugins.plumber())
-        .pipe(plugins.sourcemaps.init())
-        .pipe(plugins.if(minify, plugins.uglify()))
-        .pipe(plugins.concat('bootstrap.js'))
-        .pipe(plugins.sourcemaps.write('./'))
-        .pipe(gulp.dest('./web/js/' + env));
+        .pipe(gulp.dest(files.css.destination));
 });
 
 gulp.task('build-js-core', function() {
     return gulp.src(files.js.core)
         .pipe(plugins.plumber())
         .pipe(plugins.sourcemaps.init())
-        .pipe(plugins.if(minify, plugins.uglify()))
-        .pipe(plugins.concat('core.js'))
-        .pipe(plugins.jshint())
+            .pipe(plugins.uglify())
+            .pipe(plugins.concat('core.js'))
+            .pipe(plugins.jshint())
         .pipe(plugins.sourcemaps.write('./'))
-        .pipe(gulp.dest('./web/js/' + env));
+        .pipe(gulp.dest(files.js.destination));
 });
-
-gulp.task('build-js-user', function() {
-    return gulp.src(files.js.user)
-        .pipe(plugins.plumber())
-        .pipe(plugins.sourcemaps.init())
-        .pipe(plugins.if(minify, plugins.uglify()))
-        .pipe(plugins.concat('user.js'))
-        .pipe(plugins.jshint())
-        .pipe(plugins.sourcemaps.write('./'))
-        .pipe(gulp.dest('./web/js/' + env));
-});
-
-// Without this function exec() will not show any output
-var logStdOutAndErr = function (err, stdout, stderr) {
-    console.log(stdout + stderr);
-};
